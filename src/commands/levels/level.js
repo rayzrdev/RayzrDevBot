8561a78e7b1e197db2b745eb114d5cca62f5ebc9
Rayzr522 <rayzr522@gmail.com>
2017-05-31 12:03:29 -0400

    Emergency level editing command

---
+++ src/commands/levels/level.js
@@ -0,0 +1,28 @@
+exports.init = bot => {
+    this.levels = bot.managers.get('levels');
+};
+
+exports.run = (bot, msg, args) => {
+    if (args.length < 2) {
+        throw 'Please provide a user and an XP amount';
+    }
+
+    const member = msg.mentions.members.first();
+    const level = parseInt(args[1]);
+
+    if (isNaN(level)) {
+        throw `\`${args[1]}\` is not a valid XP amount.`;
+    }
+
+    const xp = this.levels.xpFromLevel(level - 1);
+    this.levels.setXP(member.id, xp).then(() => {
+        msg.channel.send(`:white_check_mark: Set ${member}'s level to ${level}`);
+    }
+};
+
+exports.info = {
+    name: 'level',
+    usage: 'level <user> <amount>',
+    description: 'Sets the rank of a user to a certain level',
+    perms: 'MANAGE_GUILD'
+};
