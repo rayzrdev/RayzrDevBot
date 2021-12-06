fdb3abd8e2e7898cbb547578505596c8a4e2918d
Ethan Matten <24141433+Madzahttr@users.noreply.github.com>
2021-12-06 18:50:25 +1300

    hopefully fix the online counter

--- src/bot.js
+++ src/bot.js
@@ -10,15 +10,16 @@ global.settings = {
 
 global.factory = require('./factory');
 
+const intents = new Intents([
+    Intents.NON_PRIVILEGED,
+    "GUILD_MEMBERS", 
+]);
+
 const bot = new Discord.Client({
     ws: {
-        intents: [
-            'DIRECT_MESSAGES',
-            'GUILD_MESSAGES',
-            'GUILD_MEMBERS',
-            'GUILDS',
-        ]
-    }
+        intents
+    },
+    fetchAllMembers: true
 });
 
 const managers = bot.managers = new ManagerHandler()
